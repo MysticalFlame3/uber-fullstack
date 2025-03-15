9280c8e401fd6f7ca031f057939182dbeb6c270b
MysticalFlame3 <suryaraj.9274@gmail.com>
2025-03-15 21:35:52 +0530

    cors updated

--- Backend/app.js
+++ Backend/app.js
@@ -11,9 +11,6 @@ const mapsRoutes = require('./routes/maps.routes');
 const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
-
-app.use(cors());
-import cors from "cors";
 app.use(cors({ origin: "https://cloneubrfullstack.netlify.app", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
